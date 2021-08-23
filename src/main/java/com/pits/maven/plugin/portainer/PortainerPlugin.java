77720bdcadb7b1ad6b18261efcb0ab7e06ccebea
Maksim Gromov <m.gromov@premiumitsolution.com>
2021-08-23 14:42:52 +0300

    FEMIDA-54 Реализация

--- src/main/java/com/pits/maven/plugin/portainer/PortainerPlugin.java
+++ src/main/java/com/pits/maven/plugin/portainer/PortainerPlugin.java
@@ -4,8 +4,16 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.pits.maven.plugin.data.docker.dto.RestartPolicy;
 import com.pits.maven.plugin.data.portainer.ApiClient;
+import com.pits.maven.plugin.data.portainer.ApiException;
+import com.pits.maven.plugin.data.portainer.controller.AuthApi;
+import com.pits.maven.plugin.data.portainer.controller.EndpointsApi;
+import com.pits.maven.plugin.data.portainer.dto.AuthenticateUserRequest;
+import com.pits.maven.plugin.data.portainer.dto.AuthenticateUserResponse;
+import com.pits.maven.plugin.data.portainer.dto.EndpointSubset;
 import com.pits.maven.plugin.portainer.api.PortainerDockerApi;
 import java.net.URL;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -79,11 +87,16 @@ public class PortainerPlugin extends AbstractMojo {
 
     getLog().info(String.format("Deploy image '%s' to portainer '%s' with endpoint '%s'", dockerImageName, portainerApiUrl, portainerEndPointName));
 
-    getLog().info("PitS Portainer Plugin Finish");
-
     getLog().info("Initialize portainerApi");
     initPortainerApi();
 
+    getLog().info("Authenticate portainerApi");
+    String apiToken = authenticate();
+
+    getLog().info("Determine endpoint");
+    Integer endPointId = determineEndPoint();
+
+    getLog().info("PitS Portainer Plugin Finished");
   }
 
   private void initPortainerApi() {
@@ -119,5 +132,32 @@ public class PortainerPlugin extends AbstractMojo {
         .client(okHttpClient)
         .build();
     portainerDockerApi = retrofit.create(PortainerDockerApi.class);
+  }
+
+  private String authenticate() throws MojoFailureException {
+    try {
+      AuthApi authApi = new AuthApi(portainerApiClient);
+      AuthenticateUserRequest authenticateUserRequest = new AuthenticateUserRequest()
+          .username(portainerLogin)
+          .password(portainerPassword);
+
+      AuthenticateUserResponse response = authApi.authenticateUser(authenticateUserRequest);
+      portainerApiClient.addDefaultHeader("Authorization", "Bearer " + response.getJwt());
+      return response.getJwt();
+    } catch (ApiException error) {
+      throw new MojoFailureException("Error while authenticate for Portainer", error);
+    }
+  }
+
+  private Integer determineEndPoint() throws MojoFailureException {
+    try {
+      EndpointsApi endpointsApi = new EndpointsApi(portainerApiClient);
+      List<EndpointSubset> endpointList = endpointsApi.endpointList();
+      Optional<EndpointSubset> endpointSubsetOptional = endpointList.stream()
+          .filter(endpointSubset -> endpointSubset.getName() != null && endpointSubset.getName().equals(portainerEndPointName)).findFirst();
+      return endpointSubsetOptional.orElseThrow(() -> new MojoFailureException("Can't found endpoint by name:" + portainerEndPointName)).getId();
+    } catch (ApiException error) {
+      throw new MojoFailureException("Error while determine endPoint inside Portainer", error);
+    }
   }
 }
